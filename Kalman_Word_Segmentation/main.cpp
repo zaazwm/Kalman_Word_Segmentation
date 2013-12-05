3450692373496c65543b5dc5a10369b60fcba67c
Zhu Weimeng <zaa@Zhu-WeimengdeMacBook-Pro.local>
2013-12-05 23:54:35 +0100

    Initial Commit

---
+++ Kalman_Word_Segmentation/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Kalman_Word_Segmentation
+//
+//  Created by Zhu Weimeng on 13-12-5.
+//  Copyright (c) 2013年 Zhu Weimeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
